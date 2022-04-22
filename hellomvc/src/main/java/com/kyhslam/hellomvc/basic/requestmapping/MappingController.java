888e96f496a8febcde5d8cef450e3afb851b6e80
kyhslam <kyhslam@gmail.com>
2022-04-22 16:54:16 +0900

    요청 매핑

---
+++ hellomvc/src/main/java/com/kyhslam/hellomvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,42 @@
+package com.kyhslam.hellomvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     *
+     * @param data
+     * @return
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsume() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
