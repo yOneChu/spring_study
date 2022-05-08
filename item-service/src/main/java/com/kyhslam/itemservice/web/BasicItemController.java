d59da7761274edaa0badeab61355b72df2f6e4ec
kyhslam <kyhslam@gmail.com>
2022-05-08 09:15:32 +0900

    item update

--- item-service/src/main/java/com/kyhslam/itemservice/web/BasicItemController.java
+++ item-service/src/main/java/com/kyhslam/itemservice/web/BasicItemController.java
@@ -71,6 +71,19 @@ public class BasicItemController {
     }
 
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+        //return "basic/editForm";
+    }
 
     /**
      * 테스트용 데이터 추가
