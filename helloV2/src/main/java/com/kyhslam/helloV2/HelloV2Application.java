1921e7b40f891eebfe4319a45deddf6fa9c228ca
kyhslam <kyhslam@gmail.com>
2022-05-11 08:53:40 +0900

    study thymeleaf

---
+++ helloV2/src/main/java/com/kyhslam/helloV2/HelloV2Application.java
@@ -0,0 +1,13 @@
+package com.kyhslam.helloV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloV2Application.class, args);
+    }
+
+}
---
+++ helloV2/src/test/java/com/kyhslam/helloV2/HelloV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyhslam.helloV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
